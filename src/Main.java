c0ccb00731e5f54bdfb06691f62fac886270e0c1
luca <lucav48@gmail.com>
2025-11-28 13:08:29 +0100

    Calcolatrice fantastica

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("===CALCOLATRICE===");
+        System.out.println("Primo operando:");
+        double a = scanner.nextDouble();
+        System.out.println("Secondo operando:");
+        double b = scanner.nextDouble();
+
+        System.out.println("Operazione (+, -, *, /)");
+        String op = scanner.next();
+        double result = 0;
+        switch (op){
+            case "+":
+                result = a + b;
+            break;
+            case "-":
+                result = a-b;
+            break;
+            case "*":
+                result = a*b;
+            break;
+            case "/":
+                result = a/b;
+            break;
+        }
+        System.out.println("Risultato: " + result);
+    }
+}
